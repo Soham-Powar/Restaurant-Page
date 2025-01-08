b2724c98983ffe4eb0ec3a92ffe24fe27df82727
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-08 13:44:26 +0530

    Add style to selected tab in navbar.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,34 @@ import './styles.css'
 
 import { renderHome } from "./pages/home/home.js";
 import { renderMenu } from "./pages/menu/menu.js";
+// import { renderAbout } from "./pages/about/about.js";
 
 let navBtns = document.querySelectorAll('nav button');
 
 navBtns.forEach((btn) => {
 	console.log(btn);
 	btn.addEventListener('click', (e) => {
+
+		navBtns.forEach((btn) => {
+			btn.removeAttribute("class");
+		});
+
 		if(btn.innerHTML === 'Home') {
+			btn.classList.add('btn-selected');
 			renderHome();
 		}
 		else if(btn.innerHTML === 'Menu') {
+			btn.classList.add('btn-selected');
 			renderMenu();
 		}
+		// else if(btn.innerHTML === 'About') {
+		// 	btn.classList.add('btn-selected');
+		// 	renderAbout();
+		// }
 	});
 });
 
 document.addEventListener("DOMContentLoaded", () => {
-  renderHome();
+	navBtns[0].classList.add('btn-selected');
+	renderHome();
 });
