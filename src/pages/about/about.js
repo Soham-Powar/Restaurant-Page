39a9281ece95e2bc25cbb5b5d14ef208b9b8b50e
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-08 14:05:58 +0530

    Add styles to project.

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,9 +1,25 @@
 import './about.css'
+import chefImg from './about.jpeg'
 
 const container = document.querySelector('#content');
 const newDiv = document.createElement('div');
 newDiv.classList.add('about-contents')
-newDiv.innerHTML = `hello`
+newDiv.innerHTML = `
+	<img src="${chefImg}" alt="">
+	<div>
+		<h2>About Us</h2>
+		<p>
+			At La Belle Vie Bistro, we are more than just a restaurant, we are a movement.
+			Founded by a group of passionate individuals, our mission is to transform the way people experience Japanese cuisine.
+			We believe in sustainability, mindfulness, and honoring the planet while enjoying the finest flavors.
+
+			Our founders, who have been at the forefront of plant-based culinary innovation, set out to combine the elegance and tradition of Japanese dishes with the power of vegan ingredients.
+			Their vision is to create a dining experience that not only nourishes the body but also the soul, making every meal an opportunity to connect with nature.
+
+			Join us in our journey, where every dish is a celebration of life, health, and our deep respect for the environment.
+		</p>
+	</div>
+`;
 
 export function renderAbout () {
 	container.innerHTML = '';
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -6,15 +6,16 @@ const newDiv = document.createElement('div');
 newDiv.classList.add('home-contents')
 newDiv.innerHTML = `<img src="${restaurantImg}" alt="">
 		<div>
-		<h1>My Restaurant</h1>
-		<p>Welcome to [Your Restaurant's Name], where every meal is crafted with love and passion. Our menu features
-			fresh,
-			high-quality ingredients, blending traditional flavors with a modern twist. Whether you’re here for a cozy
-			dinner, axx
-			family gathering, or a casual outing, our warm ambiance and friendly service will make your visit
-			unforgettable. Come
-			savor delicious dishes, refreshing beverages, and a dining experience like no other at [Your Restaurant's
-			Name].</p> </div>`
+			<h1>La Belle Vie Bistro</h1>
+			<p>Welcome to La Belle Vie Bistro, where every meal is crafted with love and passion. Our menu features
+				fresh,
+				high-quality ingredients, blending traditional flavors with a modern twist. Whether you’re here for a cozy
+				dinner, axx
+				family gathering, or a casual outing, our warm ambiance and friendly service will make your visit
+				unforgettable. Come
+				savor delicious dishes, refreshing beverages, and a dining experience like no other at La Belle Vie Bistro.
+			</p>
+		</div>`
 
 export function renderHome () {
 	container.innerHTML = '';
