31cc2c7c40907cbe33cd5cbfeaf7990b1582b2b9
Soham Powar <173037437+Soham-Powar@users.noreply.github.com>
2025-01-09 13:00:45 +0530

    Change source map for development mode.

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,7 +3,7 @@
 
 module.exports = merge(common, {
   mode: "development",
-  devtool: "inline-source-map",
+  devtool: "eval-source-map",
   devServer: {
 	watchFiles: ["./src/template.html"],
   },
